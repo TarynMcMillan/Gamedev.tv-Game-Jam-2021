d3c77a9cf0c76aaf6fa5c8b3384c8c52ddee871f
Taryn M <taryn.mcmillan@gmail.com>
2021-05-21 18:59:03 -0400

    Trying to figure out layer sorting.

---
+++ Assets/Grave.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grave : MonoBehaviour
+{
+    public void DigGrave()
+    {
+        // play dig sound
+        print("Digging grave");
+    }
+}
