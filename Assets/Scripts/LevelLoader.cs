413244b77b2dbfc23445e350e482eed4a01f351d
Taryn M <taryn.mcmillan@gmail.com>
2021-05-28 14:45:45 -0400

    Adding UI sounds to game.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,20 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField] AudioClip winSFX;
+    [SerializeField] AudioClip UISFX;
+    [SerializeField] GameObject roundCompletePanel;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void StartRoundCompleteSequence()
     {
-        this.gameObject.SetActive(true);
+        roundCompletePanel.SetActive(true);
+        audioSource.PlayOneShot(winSFX, 1f);
         // todo make sure particle effects finish before time stops
     }
     public void LoadGame()
     {
         Time.timeScale = 1;
+        PlayUISFX();
         SceneManager.LoadScene("Graveyard");
         // todo add scene fade
     }
     public void LoadMainMenu()
     {
+        PlayUISFX();
         SceneManager.LoadScene("Splash Screen");
+    }
+
+    public void OpenHelpPanel()
+    {
+        PlayUISFX();
+    }
+
+    public void PlayUISFX()
+    {
+        print("playing UI SFX");
+        audioSource.PlayOneShot(UISFX, 1f);
     }
 
 }
